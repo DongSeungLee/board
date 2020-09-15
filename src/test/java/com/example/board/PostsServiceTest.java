2ca3951b000b3b75c736b1d72c1402b2c1dd143c
tmd3282 <tmd3282@nhn.com>
2020-09-15 13:58:57 +0900

    [DongSeungeLee] develop : add verify code in PostsServiceTest

--- src/test/java/com/example/board/PostsServiceTest.java
+++ src/test/java/com/example/board/PostsServiceTest.java
@@ -1,5 +1,6 @@
 package com.example.board;
 
+import com.example.board.domain.posts.Posts;
 import com.example.board.domain.posts.PostsRepository;
 import com.example.board.service.posts.PostsService;
 import org.junit.Test;
@@ -8,7 +9,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.mockito.Mockito.when;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 public class PostsServiceTest {
@@ -25,6 +28,23 @@ public class PostsServiceTest {
         when(mockPostsRepository.findById(id)).thenThrow(IllegalArgumentException.class);
         // then
         postsService.findById(id);
+
+    }
+
+    @Test
+    public void testFindById() {
+        long id = 2L;
+        Optional<Posts> post = Optional.ofNullable(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+        when(mockPostsRepository.findById(id)).thenReturn(post);
+        //then
+        postsService.findById(id);
+
+        // assert
+        verify(mockPostsRepository, times(1)).findById(id);
 
     }
 }
--- src/test/java/com/example/board/TestTDD2.java
+++ src/test/java/com/example/board/TestTDD2.java
@@ -8,13 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.hamcrest.Matchers.equalTo;
 
 public class TestTDD2 {
 
     @Autowired
     private MockMvc mvc;
+
     private User user;
+
+    public TestTDD2() {
+        System.out.println("TestTDD2 : " + this);
+    }
 
     @Before
     public void setUp() {
@@ -30,14 +34,17 @@ public class TestTDD2 {
         System.out.println(Integer.parseInt(a));
         System.out.println("testNumberFormatException: " + user);
     }
+
     @Test
     public void testA() {
         System.out.println("testA: " + user);
     }
+
     @Test
-    public void testB(){
+    public void testB() {
         // testA와의 user reference가 다른 것을 확인할 수 있다.
         System.out.println("testB: " + user);
         assertThat(1).isEqualTo(1);
+        assertThat(user.getEmail()).startsWith("tmd");
     }
 }

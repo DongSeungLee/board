69c4796441358ff7c4fa0d79e8f40d2680c46646
tmd3282 <tmd3282@nhn.com>
2020-09-25 14:29:26 +0900

    [DongSeungeLee] develop : fix sun.text.normalizer

---
+++ src/main/java/com/example/board/ProxyRequestFilter.java
@@ -0,0 +1,47 @@
+package com.example.board;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.slf4j.MDC;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+public class ProxyRequestFilter extends OncePerRequestFilter {
+    private final String requestIdHeaderName;
+    // logback-spring.xml에서 http::x-request-id로 가지고 올 수 있다.
+    public final String MDC_REQUEST_ID = "http::x-request-id";
+    private final String MDC_METHOD = "MDCMethod";
+    public ProxyRequestFilter(String requestIdHeaderName) {
+        this.requestIdHeaderName = requestIdHeaderName;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        try {
+            String requestId = request.getHeader(requestIdHeaderName);
+            if (StringUtils.isEmpty(requestId)) {
+                requestId = Base64.encodeBase64String(UUID.randomUUID().toString().getBytes()).concat(".G");
+                log.info("RequestId is Empty. generate new RequestId({})", requestId);
+            }
+            log.info("Put requestId(MDC) : {}", requestId);
+            MDC.put(MDC_REQUEST_ID, requestId);
+            MDC.put("hoho","HOHO");
+            MDC.put(MDC_METHOD,request.getMethod());
+            filterChain.doFilter(request, response);
+        } finally {
+            MDC.remove(MDC_REQUEST_ID);
+            MDC.remove(MDC_METHOD);
+        }
+
+    }
+}
+
---
+++ src/main/java/com/example/board/WebMvcConfig.java
@@ -0,0 +1,44 @@
+package com.example.board;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    private Interceptor interceptor;
+
+    public WebMvcConfig(Interceptor interceptor) {
+        this.interceptor = interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor)
+                .addPathPatterns("/**")
+
+                .excludePathPatterns("/test/**");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/hihi").setViewName("forward:/Favorite/bookmark");
+    }
+
+    // log에 http.x-request-id를 찍고 싶은데 어떻게 하는 지 아직 모르겠다......
+    // 근데 쉽게 함 ^_^
+    @Bean
+    public ProxyRequestFilter requestIdFilter(@Value(value = "${proxy.server.request-id-header-name}") String requestIdHeaderName) {
+        ProxyRequestFilter orderedProxyRequestIdFilter = new ProxyRequestFilter(requestIdHeaderName);
+
+        return orderedProxyRequestIdFilter;
+    }
+
+}
+
